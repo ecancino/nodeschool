e267606a0f800d0c8f40c7797f49b511c2eee8a6
Eduardo Cancino <eduardo.cancino@globant.com>
2014-10-21 14:19:37 -0500

    JAVASCRIPTING

---
+++ javascripting/09_id_statement.js
@@ -0,0 +1,9 @@
+var fruit = 'orange', message;
+
+if (fruit.length > 5) {
+  message = 'The fruit name has more than five characters.';
+} else {
+  message = 'The fruit name has less or equal than five characters.';
+}
+
+console.log(message);
